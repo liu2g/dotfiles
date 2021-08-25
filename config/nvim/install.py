43a37681afe48bb7a92ec4459cfbfaafc237d3ea
Zuguang Liu <liu2z2@mail.uc.edu>
2021-08-25 12:25:28 -0500

    Fix install script when path already exists

--- config/nvim/install.py
+++ config/nvim/install.py
@@ -9,5 +9,7 @@ with open(str(file_path.parents[2] / "meta.json")) as f:
 linked_config_path = Path.home() / config_dict["config_path"] / "nvim" 
 real_config_path = file_path.parents[0]
 
+linked_config_path.unlink(missing_ok=True)
+
 linked_config_path.symlink_to(real_config_path,
         target_is_directory=True)
--- config/zsh/install.py
+++ config/zsh/install.py
@@ -9,10 +9,14 @@ with open(str(file_path.parents[2] / "meta.json")) as f:
 linked_config_path = Path.home() / config_dict["config_path"] / "zsh" 
 real_config_path = file_path.parents[0]
 
+linked_config_path.unlink(missing_ok=True)
+
 linked_config_path.symlink_to(real_config_path,
         target_is_directory=True)
 
 linked_rc_path = Path.home() / ".zshrc"
 real_rc_path = file_path.parents[0] / "zshrc"
+
+linked_rc_path.unlink(missing_ok=True)
 
 linked_rc_path.symlink_to(real_rc_path)
