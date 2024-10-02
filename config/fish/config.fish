734e88e3a9ee7844e6a1b2fdd24acc0db4923080
star-platinum <zliu@mail.bradley.edu>
2024-10-01 21:41:16 -0500

    Update fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,6 +7,7 @@ if status is-interactive
     alias ls="eza --group-directories-first --icons"
     alias nv="nvim"
     alias wdump="lynx -width 100000 -dump"
+    abbr -a !! --position anywhere --function last_history_item
     function chpwd --on-variable PWD --description 'handler of changing $PWD'
         ls -G
     end
