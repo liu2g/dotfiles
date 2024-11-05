08b5756fa3c0b08707ffbe478a8465b30416e369
star-platinum <zliu@mail.bradley.edu>
2024-11-05 01:26:30 -0600

    Modified: config/fish/config.fish config/polybar/config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,7 +7,7 @@ if status is-interactive
     alias ls="eza --group-directories-first --icons"
     alias nv="nvim"
     alias wdump="lynx -width 100000 -dump"
-    abbr --add trapint --position command trap '"jobs -p | xargs kill ; trap - INT"' INT ';' 
+    abbr --add mt --position command trap '"jobs -p | xargs kill ; trap - INT"' INT ';' 
     abbr -a !! --position anywhere --function last_history_item
     function chpwd --on-variable PWD --description 'handler of changing $PWD'
         ls -G
@@ -17,3 +17,11 @@ if status is-interactive
     end
     zoxide init fish | source
 end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f $HOME/.local/lib/miniforge3/bin/conda
+    eval $HOME/.local/lib/miniforge3/bin/conda "shell.fish" "hook" $argv | source
+end
+# <<< conda initialize <<<
+
